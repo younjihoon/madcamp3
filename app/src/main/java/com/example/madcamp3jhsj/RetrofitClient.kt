fd5a367102d243ad975fc3bfc05d1e315ec97b88
younjihoon <jihoon9835@gmail.com>
2025-01-13 22:52:36 +0900

    [FEAT] api emul init

--- app/src/main/java/com/example/madcamp3jhsj/RetrofitClient.kt
+++ app/src/main/java/com/example/madcamp3jhsj/RetrofitClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "http://localhost:8080"  // ✅ Change this
+    private const val BASE_URL = "http://10.0.0.2:8080"  // ✅ Change this
 
     private val retrofit: Retrofit by lazy {
         Retrofit.Builder()
